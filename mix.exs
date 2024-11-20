32950523ad73385f02acd87143ef3c470bb8d62a
5HT <maxim@synrc.com>
2024-11-20 18:44:18 +0200

    packaging

--- mix.exs
+++ mix.exs
@@ -1,27 +1,24 @@
 defmodule COSE.MixProject do
   use Mix.Project
-
+  def application do [ extra_applications: [:logger, :crypto] ] end
   def project do
     [
       app: :cose,
       version: "0.1.0",
-      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
-  end
-
-  def application do
-    [
-      extra_applications: [:logger, :crypto]
-    ]
-  end
-
-  defp deps do
-    [
-      {:b58, "~> 1.0"},
-      {:cbor, "~> 1.0.0"},
-      {:hkdf_erlang, "~> 0.1.1"}
+      deps: [
+        {:ex_doc, ">= 0.0.0", only: :dev},
+        {:b58, "~> 1.0"},
+        {:cbor, "~> 1.0.0"},
+        {:hkdf_erlang, "~> 0.1.1"}
+      ],
+      package: [
+        name: :cose,
+        files: ~w(lib mix.exs LICENSE README.md),
+        licenses: ["MIT"],
+        maintainers: ["Namdak Tonpa"],
+        links: %{"GitHub" => "https://github.com/chat-x509/cose"}
+      ],
     ]
   end
 end
